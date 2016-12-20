d37bbdde871e6eb505ed76ecdd192e8e5535c5b3
Carlis Liu <carlisliu@live.com>
2016-12-20 12:57:01 +0800

    add implementation

---
+++ index.js
@@ -0,0 +1,23 @@
+import {
+    isFunction
+} from 'js-is-type';
+
+export default function wrap(target, name, wrapper) {
+    if (!target) {
+        return;
+    }
+    if (!wrapper) {
+        return;
+    }
+    if (!isFunction(wrapper)) {
+        return;
+    }
+    var original = target[name];
+    if (original && original._wrapped) {
+        return;
+    }
+    var wrapped = wrapper(original);
+    wrapped._wrapped = true;
+    target[name] = wrapped;
+    return wrapped;
+}
